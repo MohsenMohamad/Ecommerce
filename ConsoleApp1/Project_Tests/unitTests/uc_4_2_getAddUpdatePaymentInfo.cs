6ab0cefc101c04f85d359606e225e15ae3caeae2
shady <obeed@post.bgu.ac.il>
2021-04-11 16:23:52 +0300

    bad cases added to 4.2 test

--- ConsoleApp1/Project_Tests/unitTests/uc_4_2_getAddUpdatePaymentInfo.cs
+++ ConsoleApp1/Project_Tests/unitTests/uc_4_2_getAddUpdatePaymentInfo.cs
@@ -15,6 +15,8 @@ namespace Tests
 
         private static string initialPolicy;
         private static string storeName;
+        private List<string> info;
+        private List<string> emptyList;
         //private static string selling pol
         [SetUp]
         public void Setup()
@@ -23,30 +25,44 @@ namespace Tests
             guest = new Guest();
             initialPolicy = "10% sales";
             storeName = "helloMarket";
-            
+            emptyList = new List<string>();
             admin = new SystemAdmin();
             initSystem(admin);
             OpenStore(user,initialPolicy, storeName);
+            info = new List<string>();
         }
 
         [Test]
-        public void Test()
+        public void TestGet()
         {
-            
             //check get after init the store policy in the constructor
-            Assert.Equals(new List<string> (), getPaymentInfo(user,storeName));
-            
-            List<string> info = new List<string>();
+            Assert.Equals(emptyList, getPaymentInfo(user,storeName));
+        }
+        
+        [Test]
+        public void TestAdd()
+        {
             string newInfo = "newInfo";
-            info.Add(newInfo);
             addPaymentInfo(user, storeName, newInfo);
             
-            //check add Payment info
+            // happy
             Assert.True(getPaymentInfo(user,storeName).Contains(newInfo));
-            info.Add("secend new info");
+            //bad
+            Assert.False(getPaymentInfo(user,storeName).Contains(""));
+        }
+        [Test]
+        public void TestUpdate()
+        {
+            string newInfo = "secend new info";
+            List<string> newinfo = new List<string>();
+            newinfo.Add(newInfo);
+            updatePaymentInfo(user, storeName, newinfo);
             
-            //check update Payment info
-            Assert.Equals(updatePaymentInfo(user,storeName,info),info);
+            //happy
+            Assert.True(getPaymentInfo(user,storeName).Contains(newInfo));
+            
+            //bad the old info is not removed
+            Assert.False(getPaymentInfo(user,storeName).Contains("newInfo"));
         }
     }
 }
