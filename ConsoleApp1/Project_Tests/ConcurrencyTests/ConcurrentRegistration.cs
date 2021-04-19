1afb94715aee2b155a216b4775d650bf2aca5d3d
MohsenMohamad <mohsenm@post.bgu.ac.il>
2021-04-19 15:33:57 +0300

    implement concurrent registration

---
+++ ConsoleApp1/Project_Tests/ConcurrencyTests/ConcurrentRegistration.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Project_tests.ConcurrencyTests
+{
+    public class ConcurrentRegistration : ATProject
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+           
+        }
+        
+        [Test]
+        public void Allow()
+        {
+            // Register two users with different names on the same time : should accept
+            
+            var result1 =false;
+            var result2 = false;
+            
+            var task1 = Task.Factory.StartNew(() => result1 = Register("shadi","12345"));
+            var task2 = Task.Factory.StartNew(() => result2 = Register("Adnan","1234"));
+
+            Task.WaitAll(task1, task2);
+
+            Assert.True(result1);
+            Assert.True(result2);
+            
+        }
+
+        [Test]
+        public void Reject()
+        {
+            // Register two users with the same name on the same time : should reject one
+            
+            var result1 =false;
+            var result2 = false;
+            
+            var task1 = Task.Factory.StartNew(() => result1 = Register("omar","54321"));
+            var task2 = Task.Factory.StartNew(() => result2 = Register("omar","11111"));
+
+            Task.WaitAll(task1, task2);
+            
+            Assert.True(result1 ^ result2); // XOR
+
+        }
+        
+    }
+}
