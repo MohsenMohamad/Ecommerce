461abcafefda8c1ae7a05c8b5d3857651e95c642
shadyObeed <obeed@post.bgu.ac.il>
2021-04-20 23:21:15 +0300

    adding dummy server

---
+++ ConsoleApp1/ServerApi/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Owin.Hosting;
+using SuperWebSocket;
+using System;
+using System.Threading;
+
+namespace ServiceApi
+{
+    class Program
+    {
+        private static WebSocketServer wsServer;
+        
+
+        static void Main(string[] args)
+        {
+            wsServer = new WebSocketServer();
+            int port = 8088;
+            wsServer.Setup(port);
+            wsServer.NewSessionConnected += WsServer_NewSessionConnected;
+            wsServer.NewMessageReceived += WsServer_NewMessageReceived;
+            wsServer.NewDataReceived += WsServer_NewDataReceived;
+            wsServer.SessionClosed += WsServer_SessionClosed;
+            wsServer.Start();
+            Console.WriteLine("Server is running on port " + port + ". Press ENTER to exit....");
+            Console.ReadKey();
+            /*try
+            {
+                Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }*/
+        }
+
+        private static void WsServer_SessionClosed(WebSocketSession session, SuperSocket.SocketBase.CloseReason value)
+        {
+            Console.WriteLine("SessionClosed");
+        }
+
+        private static void WsServer_NewDataReceived(WebSocketSession session, byte[] value)
+        {
+            Console.WriteLine("NewDataReceived");
+        }
+
+        private static void WsServer_NewMessageReceived(WebSocketSession session, string value)
+        {
+            Console.WriteLine("NewMessageReceived: " + value);
+            if (value == "Hello server")
+            {
+                session.Send("Hello client");
+            }
+        }
+
+        private static void WsServer_NewSessionConnected(WebSocketSession session)
+        {
+            Console.WriteLine("NewSessionConnected");
+        }
+    }
+}
+
