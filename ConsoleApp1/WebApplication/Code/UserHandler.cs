22691cfaa77b7135624f2c9317401b9668835181
adnan shawahny <shawahny@post.bgu.ac.il>
2021-04-21 16:39:38 +0300

    add handler

---
+++ ConsoleApp1/WebApplication/Code/UserHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Code
+{
+    public class UserHandler
+    {
+    }
+
+    public int Register(string username, string password)
+    {
+        string param = string.Format("username={0}&password={1}", username, password);
+        return int.Parse(SysTools.SendApi(SysTools.Service_type.USER, "Register", param));
+    }
+
+}
