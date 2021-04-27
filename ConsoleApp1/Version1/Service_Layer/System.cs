08f000634b61d1f76293dce5f0c164ca761f4f8e
MohsenMohamad <mohsenm@post.bgu.ac.il>
2021-04-27 03:21:53 +0300

    started implementing store permissions

--- ConsoleApp1/Version1/Service_Layer/System.cs
+++ ConsoleApp1/Version1/Service_Layer/System.cs
@@ -12,22 +12,31 @@ namespace Version1.Service_Layer
     {
         public static void Main(string[] args)
         {
-            
-            var facade = new Facade();
-            
-            facade.Register("zzz", "123");
-           facade.Login("zzz", "123");
-            
-            facade.OpenShop("zzz","store1","ss");
+            /*    
+                var facade = new Facade();
+                
+                facade.Register("zzz", "123");
+               facade.Login("zzz", "123");
+                
+                facade.OpenShop("zzz","store1","ss");
+    
+                facade.AddNewProductToSystem("111", "product1", "descreption1", 2.5, new []{"cat1"});
+                facade.AddItemToStore("store1", "111", 11);
+                facade.AddProductToBasket("zzz", "store1", "111",12);
+                var basket = facade.GetUserBaskets("zzz");
+                Console.WriteLine("this is basket "+basket[0][6]);
+    
+                Console.ReadLine();
+                */
 
-            facade.AddNewProductToSystem("111", "product1", "descreption1", 2.5, new []{"cat1"});
-            facade.AddItemToStore("store1", "111", 11);
-            facade.AddProductToBasket("zzz", "store1", "111",12);
-            var basket = facade.GetUserBaskets("zzz");
-            Console.WriteLine("this is basket "+basket[0][6]);
-
-            Console.ReadLine();
+            var x = new Permissions();
+            var result = x.GetPermissions2(27);
+            foreach (var permission in result)
+            {
+                Console.WriteLine(permission);
+            }
+            
+            Console.WriteLine(result.Count);
         }
-        
     }
 }
