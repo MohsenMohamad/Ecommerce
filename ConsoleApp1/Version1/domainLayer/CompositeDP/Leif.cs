345f6dedfc6577ad1e5167aa17a8cee6259d4c86
MohsenMohamad <mohsenm@post.bgu.ac.il>
2021-05-28 23:53:40 +0300

    added composite design pattern

---
+++ ConsoleApp1/Version1/domainLayer/CompositeDP/Leif.cs
@@ -0,0 +1,24 @@
+﻿namespace Version1.domainLayer.CompositeDP
+{
+    
+    public class Leif : Component
+    {
+        
+        // The Leaf class represents the end objects of a composition. A leaf can't
+        // have any children.
+        //
+        // Usually, it's the Leaf objects that do the actual work, whereas Composite
+        // objects only delegate to their sub-components.
+        
+        
+        public override string Operation()
+        {
+            return "Leaf";
+        }
+
+        public override bool IsComposite()
+        {
+            return false;
+        }
+    }
+}
