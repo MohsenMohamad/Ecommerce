cd24b45f254ce134d13025b626728ff12972f4be
shadyObeed <obeed@post.bgu.ac.il>
2021-06-11 14:49:56 +0300

    delete unwanted

--- ConsoleApp1/Version1/DataAccessLayer/DTO_Policies.cs
+++ ConsoleApp1/Version1/DataAccessLayer/DTO_Policies.cs
@@ -8,9 +8,7 @@ namespace Version1.DataAccessLayer
     {
         //1 is public policy 2 is conditional policy
         public int Type;   // DiscountPolicy is an enum  ///// Public Policy Fields
-        public bool bound;
-        public string fromdate;
-        public string todate;
+
         public int percentage; //// between 0 and 100
         /////////////
 
@@ -20,10 +18,6 @@ namespace Version1.DataAccessLayer
         ////////////
         public int discount_policy_id;
         public string discount_description;
-        public bool Bound()
-        {
-            return bound;
-        }
 
         public DTO_Policies()
         {
@@ -31,40 +25,23 @@ namespace Version1.DataAccessLayer
         }
         private void clearPublic()
         {
-            fromdate = "";
-            todate = "";
             percentage = -1;
-            bound = false;
         }
         public int SetPublic(string from, string to, int percentage, bool bound)
         {
             if (percentage < 0 || percentage > 100)
                 return -14;
-            clearHidden();
+
             clearConditional();
             this.percentage = percentage;
             this.Type = 1;
-            if (bound)
-                this.bound = true;
-            else
-                this.bound = false;
+
             DateTime dt = new DateTime();
-            if (DateTime.TryParse(from, out dt) && DateTime.TryParse(to, out dt) && percentage <= 100 && percentage >= 0)
-            {
-                fromdate = from;
-                todate = to;
-            }
-            else
-            {
-                if (bound)
-                    return -15;
-            }
+
+
             return 1;
         }
-        private void clearHidden()
-        {
-            bound = false;
-        }
+
         private void clearConditional() { }
 
         public int SetConditional(int percentage, string condition)
@@ -72,7 +49,6 @@ namespace Version1.DataAccessLayer
             if (percentage > 100 || percentage < 0)
                 return -14;
             clearPublic();
-            clearHidden();
             //set conditional type
             this.Type = 2;
 
