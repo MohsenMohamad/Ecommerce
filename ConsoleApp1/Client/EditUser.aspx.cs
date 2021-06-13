094b8f3e1aefa478a1e2bee2d452cf7820c8415a
adnan shawahny <shawahny@post.bgu.ac.il>
2021-06-14 00:22:17 +0300

    add History and EditUser

---
+++ ConsoleApp1/Client/EditUser.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Client
+{
+    public partial class EditUser : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox3.Text = Session["username"].ToString();
+        }
+
+        protected void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- ConsoleApp1/Client/m1.Master.designer.cs
+++ ConsoleApp1/Client/m1.Master.designer.cs
@@ -96,6 +96,15 @@ namespace Client
         protected global::System.Web.UI.WebControls.Button History;
 
         /// <summary>
+        /// EditUser control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button EditUser;
+
+        /// <summary>
         /// InitSystem control.
         /// </summary>
         /// <remarks>
