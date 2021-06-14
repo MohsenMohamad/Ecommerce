70ed3a815ad71c2c944a31b8533d635e2ec3543a
mohamad <agbamoh@post.bgu.ac.il>
2021-06-15 01:17:00 +0300

    finished integration tests

--- ConsoleApp1/Project_Tests/IntegrationTests/offersPurchaseTest/offerintegrationTests.cs
+++ ConsoleApp1/Project_Tests/IntegrationTests/offersPurchaseTest/offerintegrationTests.cs
@@ -75,7 +75,7 @@ namespace Project_Tests.UnitTests.offerpurchasetest
         }
 
         [Test]
-        public void counter_test()
+        public void counter_accept_test()
         {
 
             Assert.True(GetUserNotificationsoffer("adnan").Count == 0);
@@ -91,6 +91,36 @@ namespace Project_Tests.UnitTests.offerpurchasetest
 
             Assert.True(GetUserNotificationsoffer("mohamad").Count == 1);
             acceptoffer("2", "8", "adnan", "AdnanStore", 1, "mohamad");
+            Assert.True(GetUserNotificationsoffer("adnan").Count == 0);
+
+            Assert.True(GetUserNotificationsoffer("yara").Count == 0);
+
+            Assert.True(GetUserNotificationsoffer("mohamad").Count == 0);
+
+            Assert.True(GetUserNotifications("mohamad").Count == 2);
+
+            Assert.True(GetUserNotifications("yara").Count == 2);
+
+            Assert.True(GetUserNotifications("adnan").Count == 2);
+        }
+
+        [Test]
+        public void counter_reject_test()
+        {
+
+            Assert.True(GetUserNotificationsoffer("adnan").Count == 0);
+            Assert.True(GetUserNotificationsoffer("yara").Count == 0);
+            Recieve_purchase_offer("mohamad", "AdnanStore", "5", "2", 1);
+            Assert.True(GetUserNotificationsoffer("adnan").Count == 1);
+            Assert.True(GetUserNotificationsoffer("yara").Count == 1);
+            CounterOffer("2", "8", "mohamad", "AdnanStore", 1, "adnan", "5");
+
+            Assert.True(GetUserNotificationsoffer("adnan").Count == 0);
+
+            Assert.True(GetUserNotificationsoffer("yara").Count == 0);
+
+            Assert.True(GetUserNotificationsoffer("mohamad").Count == 1);
+            rejectoffer("2", "8", "adnan", "AdnanStore", 1, "mohamad");
             Assert.True(GetUserNotificationsoffer("adnan").Count == 0);
 
             Assert.True(GetUserNotificationsoffer("yara").Count == 0);
