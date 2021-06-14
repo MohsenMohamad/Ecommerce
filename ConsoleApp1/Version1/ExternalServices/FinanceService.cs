0be88a3b3f70d01c2c0bf25682f252388eda213c
MohsenMohamad <37587710+MohsenMohamad@users.noreply.github.com>
2021-06-15 02:46:03 +0300

    external system tests + timeout checks (#21)

--- ConsoleApp1/Version1/ExternalServices/FinanceService.cs
+++ ConsoleApp1/Version1/ExternalServices/FinanceService.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Flurl.Http;
 
 
@@ -7,17 +9,19 @@ namespace Version1.ExternalServices
     public class FinanceService : IFinanceServiceAdapter
     {
         private const string SystemUrl = "https://cs-bgu-wsep.herokuapp.com/";
-        private static async Task<string> AsyncHandShake()
+
+        private static async Task<string> AsyncHandShake(CancellationToken token)
         {
             var systemResponse = await SystemUrl
-                .PostUrlEncodedAsync(new { action_type = "handshake" })
+                .PostUrlEncodedAsync(new {action_type = "handshake"}, cancellationToken: token)
                 .ReceiveString();
             return systemResponse;
         }
-        
-        private static async Task<int> AsyncPay(string cardNumber, int expMonth, int expYear, string cardHolder, int cardCcv, int holderId)
+
+        private static async Task<int> AsyncPay(string cardNumber, int expMonth, int expYear, string cardHolder,
+            int cardCcv, int holderId)
         {
-            if (await AsyncHandShake() != "OK") return -2;  // connection error
+            if (await AsyncHandShake(new CancellationTokenSource().Token) != "OK") return -2; // connection error
 
             var systemResponse = await SystemUrl
                 .PostUrlEncodedAsync(new
@@ -33,10 +37,10 @@ namespace Version1.ExternalServices
                 .ReceiveString();
             return int.Parse(systemResponse);
         }
-        
+
         private static async Task<int> AsyncCancelPay(int transactionId)
         {
-            if (await AsyncHandShake() != "OK") return -2;  // connection error
+            if (await AsyncHandShake(new CancellationTokenSource().Token) != "OK") return -2; // connection error
 
             var systemResponse = await SystemUrl
                 .PostUrlEncodedAsync(new
@@ -47,17 +51,54 @@ namespace Version1.ExternalServices
                 .ReceiveString();
             return int.Parse(systemResponse);
         }
-        
+
+
         public bool Handshake()
         {
-            var task = Task.Run(async () => await AsyncHandShake());
-            var result = task.Result;
-            return result.Equals("OK");
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            const int timeout = 5000;
+
+            var task = AsyncHandShake(token);
+            var z = Task.Factory.StartNew(async () =>
+            {
+                try
+                {
+                    if (await Task.WhenAny(task, Task.Delay(timeout, token)) == task)
+                    {
+                        // Task completed within timeout.
+
+                        await task;
+                        tokenSource.Cancel();
+                    }
+                    else
+                    {
+                        // timeout / cancellation logic
+                        tokenSource.Cancel();
+                    }
+                }
+                catch (TaskCanceledException canceledException)
+                {
+                    Console.WriteLine(canceledException.Message);
+                }
+            }, token);
+
+            Task.WaitAll(z);
+
+            return task.IsCompleted && task.Result.Equals("OK");
+
+
+            /*   
+               var task = Task.Run(async () => await AsyncHandShake());
+               var result = task.Result;
+               return result.Equals("OK");
+               */
         }
 
         public int Pay(string cardNumber, int expMonth, int expYear, string cardHolder, int cardCcv, int holderId)
         {
-            var task = Task.Run(async () => await AsyncPay(cardNumber,expMonth,expYear,cardHolder,cardCcv,holderId));
+            var task = Task.Run(
+                async () => await AsyncPay(cardNumber, expMonth, expYear, cardHolder, cardCcv, holderId));
             var result = task.Result;
             return result;
         }
@@ -68,5 +109,54 @@ namespace Version1.ExternalServices
             var result = task.Result;
             return result;
         }
+        
+        // --------------------------------- for tests ---------------------------------- //
+        
+        private static async Task<string> DelayedAsyncHandShake(int timeout,CancellationToken token)
+        {
+            await Task.Delay(timeout,token);
+            var systemResponse = await SystemUrl
+                .PostUrlEncodedAsync(new {action_type = "handshake"}, cancellationToken: token)
+                .ReceiveString();
+            return systemResponse;
+        }
+        
+        public bool DelayedHandshake(int delayTime)
+        {
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            const int timeout = 2000;
+
+            var task = DelayedAsyncHandShake(delayTime,token);
+            var z = Task.Factory.StartNew(async () =>
+            {
+                try
+                {
+                    if (await Task.WhenAny(task, Task.Delay(timeout, token)) == task)
+                    {
+                        // Task completed within timeout.
+
+                        await task;
+                        tokenSource.Cancel();
+                    }
+                    else
+                    {
+                        // timeout / cancellation logic
+                        tokenSource.Cancel();
+                    }
+                }
+                catch (TaskCanceledException canceledException)
+                {
+                    Console.WriteLine(canceledException.Message);
+                }
+            }, token);
+
+            Task.WaitAll(z);
+
+            return task.IsCompleted && task.Result.Equals("OK");
+            
+        }
+
+
     }
 }
--- ConsoleApp1/Version1/ExternalServices/SupplyService.cs
+++ ConsoleApp1/Version1/ExternalServices/SupplyService.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Flurl.Http;
 
 
@@ -9,17 +11,17 @@ namespace Version1.ExternalServices
         private const string SystemUrl = "https://cs-bgu-wsep.herokuapp.com/";
 
 
-        private static async Task<string> AsyncHandShake()
+        private static async Task<string> AsyncHandShake(CancellationToken token)
         {
             var systemResponse = await SystemUrl
-                .PostUrlEncodedAsync(new { action_type = "handshake" })
+                .PostUrlEncodedAsync(new { action_type = "handshake" }, cancellationToken: token)
                 .ReceiveString();
             return systemResponse;
         }
         
         private static async Task<int> AsyncSupply(string nameF,string addressF,string cityF,string countryF,int zipF)
         {
-            if (await AsyncHandShake() != "OK") return -2;  // connection error
+            if (await AsyncHandShake(new CancellationTokenSource().Token) != "OK") return -2;  // connection error
 
             var systemResponse = await SystemUrl
                 .PostUrlEncodedAsync(new
@@ -38,7 +40,7 @@ namespace Version1.ExternalServices
         
         private static async Task<int> AsyncCancelSupply(int transactionId)
         {
-            if (await AsyncHandShake() != "OK") return -2;  // connection error
+            if (await AsyncHandShake(new CancellationTokenSource().Token) != "OK") return -2;  // connection error
 
             var systemResponse = await SystemUrl
                 .PostUrlEncodedAsync(new
@@ -53,9 +55,45 @@ namespace Version1.ExternalServices
         
         public bool Handshake()
         {
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            const int timeout = 2000;
+
+            var task = AsyncHandShake(token);
+            var z = Task.Factory.StartNew(async () =>
+            {
+                try
+                {
+                    if (await Task.WhenAny(task, Task.Delay(timeout, token)) == task)
+                    {
+                        // Task completed within timeout.
+
+                        await task;
+                        tokenSource.Cancel();
+                    }
+                    else
+                    {
+                        // timeout / cancellation logic
+                        tokenSource.Cancel();
+                    }
+                }
+                catch (TaskCanceledException canceledException)
+                {
+                    Console.WriteLine(canceledException.Message);
+                }
+            }, token);
+
+            Task.WaitAll(z);
+
+            return task.IsCompleted && task.Result.Equals("OK");
+            
+            
+            
+            /*
             var task = Task.Run(async () => await AsyncHandShake());
             var result = task.Result;
             return result.Equals("OK");
+            */
         }
 
         public int Supply(string nameF,string addressF,string cityF,string countryF,int zipF)
@@ -71,5 +109,54 @@ namespace Version1.ExternalServices
             var result = task.Result;
             return result;
         }
+        
+        // --------------------------------- for tests ---------------------------------- //
+        
+        private static async Task<string> DelayedAsyncHandShake(int timeout,CancellationToken token)
+        {
+            await Task.Delay(timeout,token);
+            var systemResponse = await SystemUrl
+                .PostUrlEncodedAsync(new {action_type = "handshake"}, cancellationToken: token)
+                .ReceiveString();
+            return systemResponse;
+        }
+        
+        public bool DelayedHandshake(int delayTime)
+        {
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            const int timeout = 2000;
+
+            var task = DelayedAsyncHandShake(delayTime,token);
+            var z = Task.Factory.StartNew(async () =>
+            {
+                try
+                {
+                    if (await Task.WhenAny(task, Task.Delay(timeout, token)) == task)
+                    {
+                        // Task completed within timeout.
+
+                        await task;
+                        tokenSource.Cancel();
+                    }
+                    else
+                    {
+                        // timeout / cancellation logic
+                        tokenSource.Cancel();
+                    }
+                }
+                catch (TaskCanceledException canceledException)
+                {
+                    Console.WriteLine(canceledException.Message);
+                }
+            }, token);
+
+            Task.WaitAll(z);
+
+            return task.IsCompleted && task.Result.Equals("OK");
+            
+        }
+
+
     }
 }
