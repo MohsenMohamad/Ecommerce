f7bda247b2b5749d2edf5a42500093b683abc585
adnan shawahny <shawahny@post.bgu.ac.il>
2021-06-15 13:39:38 +0300

    fix total price.

--- ConsoleApp1/Client/Cart.aspx.designer.cs
+++ ConsoleApp1/Client/Cart.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace Client
         protected global::System.Web.UI.WebControls.DataList Data_cart;
 
         /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
         /// LabelCreditcard control.
         /// </summary>
         /// <remarks>
